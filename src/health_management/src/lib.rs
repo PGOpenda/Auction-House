ab9e2d625319e8b0b9aae8068e9fa3476795fd36
PG Openda <piusopenda@gmail.com>
2024-02-20 10:21:00 +0300

    addition of payloads

--- src/health_management/src/lib.rs
+++ src/health_management/src/lib.rs
@@ -69,6 +69,56 @@ impl BoundedStorable for Doctor {
     const IS_FIXED_SIZE: bool = false;
 }
 
+//Represents payload for adding a patient
+#[derive(candid::CandidType, Serialize, Deserialize)]
+struct PatientPayLoad {
+    name: String,
+    date_of_birth: String, //Format: DD-MM-YYYY
+    gender: String,
+    ethncity: String,
+    address: String,
+    phone_number: String,
+    email: String, //Optional
+    next_of_kin: String,
+    kins_phone_number: String,
+}
+
+impl Default for PatientPayLoad {
+    fn default() -> Self {
+        PatientPayLoad{
+            name: String::default(),
+            date_of_birth: String::default(), //Format: DD-MM-YYYY
+            gender: String::default(),
+            ethncity: String::default(),
+            address: String::default(),
+            phone_number: String::default(),
+            email: String::default(), //Optional
+            next_of_kin: String::default(),
+            kins_phone_number: String::default(),
+        }
+    }
+}
+
+//Represents payload for adding a Doctor
+#[derive(candid::CandidType, Serialize, Deserialize)]
+struct DoctorPayLoad{
+    name: String,
+    email: String,
+    phone_number: String,
+    speciality: String,
+}
+
+impl Default for DoctorPayLoad {
+    fn default() -> Self {
+        DoctorPayLoad{
+            name: String::default(),
+            email: String::default(),
+            phone_number: String::default(),
+            speciality: String::default(),
+        }
+    }
+}
+
 //thread-local variables that will hold our canister's state
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
@@ -80,10 +130,13 @@ thread_local! {
             .expect("Cannot create a counter")
     );
 
-    static STORAGE: RefCell<StableBTreeMap<u64, Patient, Memory>> =
+    static PATIENT_STORAGE: RefCell<StableBTreeMap<u64, Patient, Memory>> =
+        RefCell::new(StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
+    ));
+
+    static DOCTOR_STORAGE: RefCell<StableBTreeMap<u64, Doctor, Memory>> =
         RefCell::new(StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
     ));
 }
-
-
